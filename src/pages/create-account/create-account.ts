ffcadca6f3942ca5af7b99e492642c10bc8462e6
cwstanfi <cwstanfi@iupui.edu>
2018-05-16 13:36:35 -0400

    commint form builder

--- src/pages/create-account/create-account.ts
+++ src/pages/create-account/create-account.ts
@@ -1,6 +1,6 @@
-import { Component } from '@angular/core';
-import { IonicPage, NavController, NavParams } from 'ionic-angular';
-import {FormGroup} from "@angular/forms";
+import {Component} from '@angular/core';
+import {IonicPage, NavController, NavParams} from 'ionic-angular';
+import {FormBuilder, FormGroup} from "@angular/forms";
 
 /**
  * Generated class for the CreateAccountPage page.
@@ -11,18 +11,23 @@ import {FormGroup} from "@angular/forms";
 
 @IonicPage()
 @Component({
-  selector: 'page-create-account',
-  templateUrl: 'create-account.html',
+    selector: 'page-create-account',
+    templateUrl: 'create-account.html',
 })
 export class CreateAccountPage {
 
     private loginForm: FormGroup;
 
-  constructor(public navCtrl: NavController, public navParams: NavParams) {
-  }
+    constructor(private formBuilder:FormBuilder, public navCtrl: NavController, public navParams: NavParams) {
 
-  ionViewDidLoad() {
-    console.log('ionViewDidLoad CreateAccountPage');
-  }
+        this.loginForm = this.formBuilder.group({
+            email: ['', Validators.compose([Validators.required, Validators.email])],
+            password: ['', Validators.required]
+        })
+    }
+
+    ionViewDidLoad() {
+        console.log('ionViewDidLoad CreateAccountPage');
+    }
 
 }
